7faa64dcc473c1c74b28e14cdf65dad211941055
Alice Diana <aliceangela89@gmail.com>
2019-11-06 10:16:02 +0100

    first commit

---
+++ booksdisplay.js
@@ -0,0 +1,19 @@
+// let url = "https://api.myjson.com/bins/zyv02;";
+
+// fetch(url, {
+//   method: "GET"
+// })
+//   .then(response => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     throw new Error(response.statusText);
+//   })
+//   .then(Data => {
+//     // jsonData is parsed json object received from url
+//     console.log(Data);
+//   })
+//   .catch(error => {
+//     // handle your errors here
+//     console.error(error);
+//   });
